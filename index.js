8c54aced353e03735cc764fab0eef5191a49d7dd
Kevin Li <chunxli@microsoft.com>
2021-10-01 14:59:09 +0800

    Pin to Top

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const { app, BrowserWindow, BrowserView } = require('electron')
+const { app, BrowserWindow, BrowserView, Menu } = require('electron')
+
+const isMac = process.platform === 'darwin'
+let win
+let isOnTop = false
 
 function createWindow () {
     app.on('window-all-closed', function () {
         if (process.platform !== 'darwin') app.quit()
     })
 
-    const win = new BrowserWindow({ width: 500, height: 500 })
+    win = new BrowserWindow({ width: 500, height: 700 })
     win.loadURL("https://translate.google.cn/?hl=en")
+    // win.setAlwaysOnTop("true")
+    Menu.setApplicationMenu()
 }
 
 
@@ -14,5 +20,64 @@ function createWindow () {
 app.whenReady().then(() => {
     createWindow()
 })
+
+
+const template = [
+    // { role: 'appMenu' }
+    ...(isMac ? [{
+      label: app.name,
+      submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'services' },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideOthers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    }] : []),
+    // { role: 'fileMenu' }
+    {
+      label: 'File',
+      submenu: [
+        isMac ? { role: 'close' } : { role: 'quit' }
+      ]
+    },
+    // { role: 'viewMenu' }
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    {
+        label: 'Tools',
+        submenu: [
+          {
+            label: 'Pin to Top',
+            click: async () => {
+            //   const { shell } = require('electron')
+            //   await shell.openExternal('https://electronjs.org')
+                win.setAlwaysOnTop(!isOnTop)
+                isOnTop = !isOnTop
+            }
+          }
+        ]
+    }
+  ]
+  
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+
 
 
